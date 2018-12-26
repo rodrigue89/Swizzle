92648be8b7756ec46d85f716e5a3c2d049c2b8ab
Happy HumanPointer <45888860+SafelySwift@users.noreply.github.com>
2018-12-26 12:44:10 -0500

    Update Contiguous Storage.swift

--- CompilingProgress/Contiguous/Contiguous Storage.swift
+++ CompilingProgress/Contiguous/Contiguous Storage.swift
@@ -40,10 +40,6 @@ public class ContiguousBuffer<Element> {
         first.deallocate()
         first = new
     }
-    public func _removeLast() {
-        last.deinitialize(count: 1)
-        last.deallocate()
-    }
     
     deinit {
         first.deinitialize(count: count)
